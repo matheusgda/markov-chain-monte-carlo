7d3950f0ddfa1d0df288475958d23bb49c3d6aed
Matheus Guedes de Andrade <matheusgandrade@poli.ufrj.br>
2018-05-02 22:49:44 -0300

    add method to parallel estimation with Monte Carlo

--- mc_estimator/__init__.py
+++ mc_estimator/__init__.py
@@ -1,5 +1,8 @@
+from threading import Thread
 
-__all__ = ["MonteCarloEstimator"]
+import numpy as np
+
+__all__ = ["MonteCarloEstimator", "AsyncParallelMonteCarloEstimator"]
 
 class MonteCarloEstimator:
 
@@ -15,3 +18,46 @@ class MonteCarloEstimator:
         for i in range(samples):
             emp_mean += self.evaluation(self.sampler())
         return emp_mean / samples
+
+
+
+class AsyncParallelMonteCarloEstimator:
+
+    def __init__(self, eval_func, sampler_func, n_threads):
+        self.evaluation = eval_func
+        self.sampler = sampler_func
+        self.n_threads = n_threads
+        self.acumulator = 0
+        # Remember that, because of GIL, only 1 thread executes python code at a time
+        #  ("things are atomic")
+
+
+    def eval_sampler(self, thread):
+        while True:
+            if self.samples[thread] > 0:
+                self.samples[thread] -= 1
+                a = self.sampler()
+                self.acumulator[thread] += self.evaluation(a)
+                # self.acumulator += self.evaluation(self.sampler())
+            else:
+                break
+
+
+    def estimate(self, samples):
+        self.acumulator = np.zeros(self.n_threads)
+        self.samples = np.ones(self.n_threads) * int(samples / self.n_threads)
+        self.samples[0] += samples % self.n_threads # the exact number of samples
+        threads = list()
+        for i in range(self.n_threads): 
+            t = Thread(target=self.eval_sampler, args=(i,))
+            t.start()
+            threads.append(t)
+        for t in threads:
+            t.join()
+        return np.sum(self.acumulator) / samples
+
+
+
+
+
+
