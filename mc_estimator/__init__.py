83b480f61786a44a3da2fef916553937506f5ef4
Matheus Guedes de Andrade <matheusgandrade@poli.ufrj.br>
2018-05-04 10:13:13 -0300

    importance sampling and samlping reuse for plotting

--- mc_estimator/__init__.py
+++ mc_estimator/__init__.py
@@ -9,8 +9,11 @@ class MonteCarloEstimator:
     # accepts function objects:
     #  Note that the sampler function must be called without any argument,
     #   simply returning a variable possible value within the domain.
-    def __init__(self, eval_func, sampler_func):
-        self.evaluation = eval_func
+    def __init__(self, eval_func, sampler_func, bias_correction=None):
+        if bias_correction != None:
+            self.evaluation = lambda x: eval_func(x) * bias_correction(x)
+        else:
+            self.evaluation = eval_func
         self.sampler = sampler_func
 
     def estimate(self, samples):
@@ -18,6 +21,15 @@ class MonteCarloEstimator:
         for i in range(samples):
             emp_mean += self.evaluation(self.sampler())
         return emp_mean / samples
+
+
+    def estimate_per_samples(self, samples):
+        emp_mean = 0
+        estimations = np.zeros(samples)
+        for i in range(samples):
+            emp_mean += self.evaluation(self.sampler())
+            estimations[i] = emp_mean / (i + 1)
+        return estimations
 
 
 
@@ -36,28 +48,39 @@ class AsyncParallelMonteCarloEstimator:
         while True:
             if self.samples[thread] > 0:
                 self.samples[thread] -= 1
-                a = self.sampler()
-                self.acumulator[thread] += self.evaluation(a)
-                # self.acumulator += self.evaluation(self.sampler())
+                self.acumulator[thread] += self.evaluation(self.sampler())
             else:
                 break
 
 
-    def estimate(self, samples):
+    # Store information to compute graph if desired
+    def store_eval_sampler(self, thread):
+        i = 0
+        while True:
+            if self.samples[thread] > 0:
+                self.samples[thread] -= 1
+                smp = self.sampler()
+                val = self.evaluation(smp)
+                self.acumulator[thread] += val
+                self.sample_inf[i] = val
+                i += 1
+            else:
+                break
+
+    def estimate(self, samples, store_inf=False):
         self.acumulator = np.zeros(self.n_threads)
-        self.samples = np.ones(self.n_threads) * int(samples / self.n_threads)
+        self.samples = np.ones(self.n_threads, dtype=int) * int(samples / self.n_threads)
         self.samples[0] += samples % self.n_threads # the exact number of samples
+        if store_inf:
+            self.sample_inf = np.zeros((self.n_threads, self.samples[0]))
+            eval_func = self.store_eval_sampler
+        else:
+            eval_func = self.eval_sampelr
         threads = list()
         for i in range(self.n_threads): 
-            t = Thread(target=self.eval_sampler, args=(i,))
+            t = Thread(target=eval_func, args=(i,))
             t.start()
             threads.append(t)
         for t in threads:
             t.join()
         return np.sum(self.acumulator) / samples
-
-
-
-
-
-
